7a231b38f456b00b09ad07348328ca8bce007a20
yunseong9999 <dbsdltjd3701@naver.com>
2021-11-03 04:21:09 +0900

    feat : first pass, second pass 구현 완료.

--- Pass.java
+++ Pass.java
@@ -7,23 +7,24 @@ import java.util.Map;
 public class Pass {
     public static void main(String[] args) throws IOException {
         short[] memory = new short[4096];
-        HashMap<String, Integer> sat = new HashMap<String, Integer>();
+        HashMap<String, String> sat = new HashMap<String, String>();
         BufferedReader br = new BufferedReader(new FileReader("input_example.txt"));
         BufferedReader sbr = new BufferedReader(new FileReader("input_example.txt"));
 
         int LC = 0;
         firstPass(memory, sat, LC, br);
-        for (Map.Entry<String, Integer> element : sat.entrySet()) {
+        secondPass(memory, sat, LC, sbr);
+        
+        for (Map.Entry<String, String> element : sat.entrySet()) {
             System.out.println(element.getKey() + " : " + element.getValue());
         }
         for(int i=0; i<10; i++){
-            System.out.println("Memory Idx:" + i + " Value:" + memory[i]);
+            System.out.println("Memory Idx:" + i + " Value:" + Integer.toHexString(memory[i]));
         }
-        secondPass(memory, sat, LC, sbr);
 
     }
 
-    public static void firstPass(short[] memory, HashMap<String, Integer> sat, int LC, BufferedReader br) throws IOException {
+    public static void firstPass(short[] memory, HashMap<String, String> sat, int LC, BufferedReader br) throws IOException {
         String commandLine;
         LC = 0;
         while(true){
@@ -41,12 +42,18 @@ public class Pass {
 
             // line of code have Labels
             if(targetSymbolicIdx != -1){
-                sat.put(commandLine.substring(0,targetSymbolicIdx), LC);
+                String LCString = "" + Integer.toString(LC);
+                while(LCString.length() != 3){
+                    LCString = "0" + LCString;
+                }
+                sat.put(commandLine.substring(0,targetSymbolicIdx), LCString);
                 if(commandLine.substring(targetSymbolicIdx + 1, targetSpaceIdx).equals("DEC")){
                     int temp = Integer.parseInt(commandLine.substring(targetSpaceIdx + 1));
                     memory[LC++] = (short)temp;
                 }
                 else if(commandLine.substring(targetSymbolicIdx + 1, targetSpaceIdx).equals("HEX")){
+                    int temp = Integer.parseInt(commandLine.substring(targetSpaceIdx + 1), 16);
+                    memory[LC++] = (short)temp;
                 }
 
             }
@@ -67,8 +74,7 @@ public class Pass {
         return;
     }
 
-
-    public static void secondPass(short[] memory, HashMap<String, Integer> sat, int LC, BufferedReader sbr) throws IOException{
+    public static void secondPass(short[] memory, HashMap<String, String> sat, int LC, BufferedReader sbr) throws IOException{
         String commandLine = "";
         LC = 0;
         while(true){
@@ -79,8 +85,8 @@ public class Pass {
 
             String comma = ",";
             String space = " ";
-            String commandOpcode = "0x";
-            String commandOperand ;
+            String secondPassCommand = "";
+            String commandOperand = "";
             boolean MRI_flag = false;
             boolean non_MRI_flag = false;
 
@@ -105,40 +111,110 @@ public class Pass {
             }
 
             // MRI
+            // 간접 주소에 따라 처리해주는 과정
             boolean indirectSignal = false;
-            if(commandLine.substring(commandLine.length()-1 , commandLine.length()).equals("I")) indirectSignal = true;
+            if(commandLine.substring(commandLine.length()-1).equals("I")) {
+                indirectSignal = true;
+                commandOperand = commandLine.substring(targetSpaceIdx + 1, targetSecondSpaceIdx);
+            }
             switch(command){
-                default:
-                    MRI_flag = true;
                 case "AND":
-                    commandOpcode = commandOpcode + (indirectSignal ? "8" : "0");
+                    secondPassCommand = secondPassCommand + (indirectSignal ? "8" : "0");
+                    MRI_flag = true;
                     break;
                 case "ADD":
-                    commandOpcode = commandOpcode + (indirectSignal ? "9" : "1");
+                    secondPassCommand = secondPassCommand + (indirectSignal ? "9" : "1");
+                    MRI_flag = true;
                     break;
                 case "LDA":
-                    commandOpcode =  commandOpcode + (indirectSignal ? "A" : "2");
+                    secondPassCommand = secondPassCommand + (indirectSignal ? "A" : "2");
+                    MRI_flag = true;
                     break;
                 case "STA":
-                    commandOpcode = commandOpcode + (indirectSignal ? "B" : "3");
+                    secondPassCommand = secondPassCommand + (indirectSignal ? "B" : "3");
+                    MRI_flag = true;
                     break;
                 case "BUN":
-                    commandOpcode = commandOpcode + (indirectSignal ? "C" : "4");
+                    secondPassCommand = secondPassCommand + (indirectSignal ? "C" : "4");
+                    MRI_flag = true;
                     break;
                 case "BSA":
-                    commandOpcode = commandOpcode + (indirectSignal ? "D" : "5");
+                    secondPassCommand = secondPassCommand + (indirectSignal ? "D" : "5");
+                    MRI_flag = true;
                     break;
                 case "ISZ":
-                    commandOpcode = commandOpcode + (indirectSignal ? "E" : "6");
+                    secondPassCommand = secondPassCommand + (indirectSignal ? "E" : "6");
+                    MRI_flag = true;
                     break;
             }
-            commandOperand = commandLine.substring(targetSpaceIdx+1, targetSecondSpaceIdx);
-            Integer symbolIdx = sat.get(commandOperand);
-            if(symbolIdx != null){
-            }else{
-
+            commandOperand = commandLine.substring(targetSpaceIdx + 1);
+            String symbolAddress = sat.get(commandOperand);
+            // if it's not symbolic
+            if(symbolAddress == null && MRI_flag){
+                secondPassCommand = secondPassCommand + commandOperand;
+                System.out.println(commandLine + " " + secondPassCommand);
+                int temp = Integer.parseInt(secondPassCommand, 16);
+                memory[LC++] = (short) temp;
+                continue;
+            } else if (symbolAddress != null && MRI_flag){
+                    secondPassCommand = secondPassCommand + symbolAddress;
+                    int temp = Integer.parseInt(secondPassCommand, 16);
+                    memory[LC++] = (short)temp;
+                    continue;
             }
 
+            // non - MRI instruction
+            int temp;
+            switch (command){
+                case "CLA":
+                    temp = Integer.parseInt("7800", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "CLE":
+                    temp = Integer.parseInt("7400", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "CMA":
+                    temp = Integer.parseInt("7200", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "CME":
+                    temp = Integer.parseInt("7100", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "CIR":
+                    temp = Integer.parseInt("7080", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "CIL":
+                    temp = Integer.parseInt("7040", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "INC":
+                    temp = Integer.parseInt("7020", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "SPA":
+                    temp = Integer.parseInt("7010", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "SNA":
+                    temp = Integer.parseInt("7008", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "SZA":
+                    temp = Integer.parseInt("7004", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "SZE":
+                    temp = Integer.parseInt("7002", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+                case "HLT":
+                    temp = Integer.parseInt("7001", 16);
+                    memory[LC++] = (short) temp;
+                    break;
+            }
         }
 
     }
